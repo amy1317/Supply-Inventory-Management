ebad931e04b563d2ef4162349997101f63a2d85f
amy1317 <fordamy953@gmail.com>
2019-11-22 09:37:35 -0500

    Initial Commit

---
+++ src/main/java/org/launchcode/Supply/Inventory/Management/SupplyInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Supply.Inventory.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyInventoryManagementApplication.class, args);
+	}
+
+}
