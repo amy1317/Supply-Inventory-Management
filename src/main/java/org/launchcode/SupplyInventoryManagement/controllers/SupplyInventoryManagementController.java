c3958d0774ccc07c4d91e4fd13375dcaf4b9b2ce
amy1317 <fordamy953@gmail.com>
2019-11-25 11:40:48 -0500

    enable vcs, create templates, create database, project set up

---
+++ src/main/java/org/launchcode/SupplyInventoryManagement/controllers/SupplyInventoryManagementController.java
@@ -0,0 +1,11 @@
+package org.launchcode.SupplyInventoryManagement.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SupplyInventoryManagementController {
+
+    /* TODO @RequestMapping */
+
+
+}
